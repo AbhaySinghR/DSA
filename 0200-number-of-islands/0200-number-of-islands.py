a8bf49e09161cae7604c4262e34be74580140a66
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2025-11-26 18:56:24 -0500

    Time: 258 ms (41.27%), Space: 20.1 MB (68.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
+
         n=len(grid)
         m=len(grid[0])
         cnt=0
         visited = [[0 for _ in range(m)] for _ in range(n)]
-
-        def bfs(i,j,visited,grid):
+        
+        def dfs(i,j,visited,grid):
             visited[i][j]=1
-            q=deque()
-            q.append((i, j))
-            while q:
-                i_d,j_d=q.popleft()
-                dirs = [(1,0), (-1,0), (0,1), (0,-1)]
-                for dr, dc in dirs:
-                    n_i = i_d + dr
-                    n_j = j_d + dc
-                    if 0 <= n_i < n and 0 <= n_j < m:
-                        if grid[n_i][n_j] == "1" and visited[n_i][n_j] == 0:
-                            visited[n_i][n_j] = 1
-                            q.append((n_i, n_j))
+            dir=[(-1,0),(1,0),(0,1),(0,-1)]
+            for r,c in dir:
+                n_i=i+r
+                n_j=j+c
+                if (n_i>=0 and n_i<len(grid)) and (n_j>=0 and n_j<len(grid[0])) and (grid[n_i][n_j]=="1") and (visited[n_i][n_j]!=1):
+                    dfs(n_i,n_j,visited,grid)
 
         for i in range(n):
             for j in range(m):
-                if grid[i][j]=="1" and visited[i][j]==0:
+                if grid[i][j]=="1" and visited[i][j]!=1:
                     cnt+=1
-                    bfs(i,j,visited,grid)
+                    dfs(i,j,visited,grid)
         
         return cnt
-
+        
