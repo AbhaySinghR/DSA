39737e7c2121e1c4a071abc4876f98bbd4dd01a0
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-19 16:52:55 -0500

    Time: 123 ms (93.21%), Space: 25.1 MB (74.33%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,20 +1,24 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        
-        max_len=0
+
+
+
         hmap={}
+
         l,r=0,0
+        max_fruits=float('-inf')
         while r<len(fruits):
+
             hmap[fruits[r]]=r
-            
+
             while len(hmap)>2:
                 a = min(hmap, key=hmap.get)
                 temp = hmap[a]
                 del hmap[a]
                 l=temp+1
+
             
-            max_len=max(max_len,r-l+1)
+            max_fruits=max(max_fruits,r-l+1)
             r+=1
-        
-        return max_len
+        return max_fruits
         
