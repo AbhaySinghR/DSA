c76184ead1384b11441b7e9fffb08e6f9380ec19
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-19 19:58:24 -0500

    Time: 184 ms (57.91%) | Memory: 31 MB (75.43%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -2,22 +2,29 @@ class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
 
 
-        l,r,=0,0
-        max_sum=0
-        final=float('-inf')
         hmap={}
+        l,r=0,0
+        max_score=float('-inf')
+        score=0
         while r<len(nums):
+
+            score+=nums[r]
+
             if nums[r] in hmap:
-                idx=hmap[nums[r]]
-                while l<=idx:
-                    max_sum-=nums[l]
-                    del hmap[nums[l]]
+                pos=hmap[nums[r]]
+                while l<=pos:
+                    score-=nums[l]
                     l+=1
 
             hmap[nums[r]]=r
-            max_sum+=nums[r]
-            final=max(final,max_sum)
+            max_score=max(max_score,score)
             r+=1
-        
-        return final
+        return max_score
+
+            
+
+
+
+
+
         
