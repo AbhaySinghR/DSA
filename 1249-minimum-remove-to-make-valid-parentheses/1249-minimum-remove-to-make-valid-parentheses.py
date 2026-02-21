396a96c383d8055be5b44754b7ce4a9c669ee122
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-21 16:32:28 -0500

    Time: 623 ms (6.51%), Space: 20.9 MB (34.1%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+
+
+        st=[]
+        for i in range(len(s)):
+            if s[i] == '(':
+                st.append(i)
+            elif s[i]==')':
+                if st and s[st[-1]]=='(':
+                    st.pop()
+                else:
+                    st.append(i)
+        a=set(st)
+        res=''
+    
+        for i in range(len(s)):
+            if i not in st:
+                res+=s[i]
+        
+        return res
+
+        
