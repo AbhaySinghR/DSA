55c0c1fdf44b2e495646e739fefe1c985c663b45
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-23 16:48:15 -0500

    Time: 4 ms (38.08%) | Memory: 19.3 MB (66.76%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,35 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
 
-        n=len(grid)
-        m=len(grid[0])
-        minute=0
-        q=deque()
-        visited=set()
 
-        for i in range(n):
-            for j in range(m):
+        m=len(grid)
+        n=len(grid[0])
+        q=deque()
+        vis=set()
+        mins=0
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j]==2:
                     q.append((i,j,0))
-                    visited.add((i,j))
-        dir = [(0,1),(1,0),(0,-1),(-1,0)]
+                    vis.add((i,j))
+
+        dir=[(0,1),(1,0),(-1,0),(0,-1)]
         while q:
             size=len(q)
             while size:
-                i,j,t = q.popleft()
-                for r,c in dir:
-                    n_i = i + r
-                    n_j = j + c
-                    if ((n_i>=0 and n_i<n) and (n_j>=0 and n_j<m)):
-                        if grid[n_i][n_j]==1 and (n_i,n_j) not in visited:
+                i,j,t=q.popleft()
+                for dr, dc in dir:
+                    n_i,n_j=dr+i,dc+j
+                    if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
+                        if grid[n_i][n_j]==1 and (n_i,n_j) not in vis:
                             grid[n_i][n_j]=2
+                            vis.add((n_i,n_j))
                             q.append((n_i,n_j,t+1))
-                            visited.add((n_i,n_j,t+1))
                 size-=1
-            minute=t
+            mins=t
         
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j]==1:
                     return -1
-        return minute
+        
+        return mins
+
+
+
+        
