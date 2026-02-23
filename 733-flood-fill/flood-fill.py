98833cc339073c96beeb79e0c929dea257290e14
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-23 14:30:07 -0500

    Time: 3 ms (16.01%) | Memory: 19.6 MB (37.44%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,20 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-     
-        def dfs(i,j,image):
+
+
+        m=len(image)
+        n=len(image[0])
+        vis=set()
+
+        q=deque()
+        start_pixel=image[sr][sc]
+        q.append((sr,sc))
+        while q:
+            r,c=q.popleft()
+            vis.add((r,c))
+            image[r][c]=color
             dir=[(0,1),(1,0),(-1,0),(0,-1)]
-            for r,c in dir:
-                n_i=i+r
-                n_j=j+c
-                if ((n_i>=0 and n_i<len(image)) and (n_j>=0 and n_j<len(image[0]))):
-                    if image[n_i][n_j]==self.init:
-                        image[n_i][n_j]=color
-                        dfs(n_i,n_j,image)        
-        self.init=image[sr][sc]
-        if self.init == color:
-            return image
-        image[sr][sc]=color
-        dfs(sr,sc,image)
+            for dr, dc in dir:
+                n_i, n_j = dr + r, dc + c 
+                if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
+                    if image[n_i][n_j]==start_pixel and (n_i,n_j) not in vis:
+                        image[n_i][n_j]=color 
+                        vis.add((n_i,n_j))
+                        q.append((n_i,n_j))
+        
         return image
         
