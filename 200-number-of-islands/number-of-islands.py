b2922f9b36954f9c9d0ce6c52c3654c93e6a45d2
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-23 13:42:30 -0500

    Time: 238 ms (73.93%) | Memory: 21.8 MB (47.28%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        n=len(grid)
-        m=len(grid[0])
-        cnt=0
-        visited = [[0 for _ in range(m)] for _ in range(n)]
-        
-        def dfs(i,j,visited,grid):
-            visited[i][j]=1
-            dir=[(-1,0),(1,0),(0,1),(0,-1)]
-            for r,c in dir:
-                n_i=i+r
-                n_j=j+c
-                if (n_i>=0 and n_i<len(grid)) and (n_j>=0 and n_j<len(grid[0])) and (grid[n_i][n_j]=="1") and (visited[n_i][n_j]!=1):
-                    dfs(n_i,n_j,visited,grid)
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]=="1" and visited[i][j]!=1:
+
+        m=len(grid)
+        n=len(grid[0])
+        vis_arr=[]
+        cnt=0
+        for i in range(m):
+            row=[]
+            for j in range(n):
+                row.append(0)
+            vis_arr.append(row)
+        
+        def nIsl(i,j,grid,vis_arr):
+            vis_arr[i][j]=1
+            q=deque()
+            q.append((i,j))
+            while q:
+                r,c= q.popleft()
+                dir=[(0,1),(1,0),(0,-1),(-1,0)]
+                for dr, dc in dir:
+                    n_r,n_c=r+dr,c+dc
+                    if (n_r>=0 and n_r<m) and (n_c>=0 and n_c <n):
+                        if grid[n_r][n_c]=="1" and vis_arr[n_r][n_c]!=1:
+                            vis_arr[n_r][n_c]=1  
+                            q.append((n_r,n_c))     
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=="1" and vis_arr[i][j]!=1:
                     cnt+=1
-                    dfs(i,j,visited,grid)
+                    nIsl(i,j,grid,vis_arr)
         
         return cnt
+
         
