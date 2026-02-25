16b01c1d469376d894f97faf9dd6b55e851aa5bf
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-24 19:16:55 -0500

    Time: 19 ms (46.27%), Space: 20.1 MB (32.94%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -2,46 +2,42 @@ class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
 
 
-        n=len(grid)
-        m=len(grid[0])
-        visited=set()
-        q=deque()
+        m=len(grid)
+        n=len(grid[0])
+        vis=set()
+        val=0
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]==0 and ((i==0 or i==n-1) or (j==0 or j==m-1)):
-                    q.append((i,j))
-                    visited.add((i,j))
-        
-        while q:
-            i,j = q.popleft()
+        def closed(i,j,vis,grid):
+            isClosed=True
+            q=deque()
+            vis.add((i,j))
+            q.append((i,j))
             dir=[(0,1),(1,0),(0,-1),(-1,0)]
-            for r,c in dir:
-                n_i = i + r
-                n_j = j + c
-                if ((0<=n_i<n) and (0<=n_j<m)) and (n_i,n_j) not in visited:
-                    if grid[n_i][n_j]==0:
-                        q.append((n_i,n_j))
-                        visited.add((n_i,n_j))
-        
-        cnt=0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]==0 and (i,j) not in visited:
-                    q=deque()
-                    q.append((i,j))
-                    visited.add((i,j))
-                    while q:
-                        curr_i,curr_j = q.popleft()
-                        dir=[(0,1),(1,0),(0,-1),(-1,0)]
-                        for r,c in dir:
-                            n_i = curr_i + r
-                            n_j = curr_j + c
-                            if ((0<=n_i<n) and (0<=n_j<m)) and (n_i,n_j) not in visited:
-                                if grid[n_i][n_j]==0:
-                                    q.append((n_i,n_j))
-                                    visited.add((n_i,n_j))
-                    cnt+=1
+            while q:
+                r,c =q.popleft()
+                if (r==0 or r==m-1) or (c==0 or c==n-1):
+                    isClosed=False
+                for dr,dc in dir:
+                    n_i,n_j=r+dr,c+dc
+                    if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
+                            if grid[n_i][n_j]==0 and (n_i,n_j) not in vis:
+                                q.append((n_i,n_j))
+                                vis.add((n_i,n_j))
             
-        return cnt
+            if isClosed==True:
+                return 1
+            else:
+                return 0
 
+            
+
+
+
+        for i in range(m):
+            for j in range(n):
+                if (i!=0 and i!=m-1) and (j!=0 and j!=n-1):
+                    if grid[i][j]==0 and (i,j) not in vis:
+                        val+=closed(i,j,vis,grid)
+                        #print (val,i,j)
+        
+        return val
