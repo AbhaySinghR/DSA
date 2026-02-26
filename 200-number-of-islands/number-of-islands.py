5814fdb96d277f8b7904e7d2b169a1a8ca365676
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-26 16:15:09 -0500

    Time: 259 ms (32.39%) | Memory: 27 MB (15.12%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-
-
         m=len(grid)
         n=len(grid[0])
-        vis_arr=[]
-        cnt=0
-        for i in range(m):
-            row=[]
-            for j in range(n):
-                row.append(0)
-            vis_arr.append(row)
-        
-        def nIsl(i,j,grid,vis_arr):
-            vis_arr[i][j]=1
+        vis=set()
+
+        def nIsl(i,j,vis,grid):
             q=deque()
             q.append((i,j))
+            cnt=0
+            vis.add((i,j))
+            dir =[(0,1),(1,0),(0,-1),(-1,0)]
             while q:
-                r,c= q.popleft()
-                dir=[(0,1),(1,0),(0,-1),(-1,0)]
+                r,c =q.popleft()
                 for dr, dc in dir:
-                    n_r,n_c=r+dr,c+dc
-                    if (n_r>=0 and n_r<m) and (n_c>=0 and n_c <n):
-                        if grid[n_r][n_c]=="1" and vis_arr[n_r][n_c]!=1:
-                            vis_arr[n_r][n_c]=1  
-                            q.append((n_r,n_c))     
+                    n_i,n_j = r+dr,c+dc
+                    if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
+                        if grid[n_i][n_j]=="1" and (n_i,n_j) not in vis:
+                            q.append((n_i,n_j))
+                            vis.add((n_i,n_j))
+            
+            return cnt+1 
+
+        res=0
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=="1" and vis_arr[i][j]!=1:
-                    cnt+=1
-                    nIsl(i,j,grid,vis_arr)
-        
-        return cnt
-
-        
+                if grid[i][j]=="1" and (i,j) not in vis:
+                    res+=nIsl(i,j,vis,grid)
+        return res
