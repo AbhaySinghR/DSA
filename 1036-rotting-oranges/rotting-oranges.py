eefa89ab6107aaa5e66a13eaa6b1b004d909ba0a
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-02-26 01:30:44 -0500

    Time: 3 ms (77.18%) | Memory: 19.3 MB (82.33%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -4,36 +4,33 @@ class Solution:
 
         m=len(grid)
         n=len(grid[0])
+        t=0
         q=deque()
         vis=set()
-        mins=0
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==2:
-                    q.append((i,j,0))
-                    vis.add((i,j))
-
+                    q.append((i,j,t))
+        
         dir=[(0,1),(1,0),(-1,0),(0,-1)]
         while q:
-            size=len(q)
-            while size:
-                i,j,t=q.popleft()
-                for dr, dc in dir:
-                    n_i,n_j=dr+i,dc+j
-                    if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
-                        if grid[n_i][n_j]==1 and (n_i,n_j) not in vis:
-                            grid[n_i][n_j]=2
-                            vis.add((n_i,n_j))
-                            q.append((n_i,n_j,t+1))
-                size-=1
-            mins=t
+            r,c,t=q.popleft()
+            vis.add((r,c))
+            for dr, dc in dir:
+                n_i,n_j=r+dr, c+dc
+                if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
+                    if grid[n_i][n_j]==1 and (n_i,n_j) not in vis:
+                        q.append((n_i,n_j,t+1))
+                        vis.add((n_i,n_j))
+        
         
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==1:
+                if grid[i][j]==1 and (i,j) not in vis:
                     return -1
         
-        return mins
+        return t
 
 
 
