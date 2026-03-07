81abba2a7d4402029caa1cf472a6d6b1383fa5ea
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-03-07 17:15:15 -0500

    Time: 3 ms (76.15%) | Memory: 19.3 MB (94.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
 
-
         m=len(grid)
         n=len(grid[0])
-        t=0
         q=deque()
+        res=0
         vis=set()
-
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==2:
-                    q.append((i,j,t))
+                if grid[i][j]==2 and (i,j) not in vis:
+                    q.append((i,j,0))
         
-        dir=[(0,1),(1,0),(-1,0),(0,-1)]
+        dir=[(1,0),(0,1),(-1,0),(0,-1)]
         while q:
-            r,c,t=q.popleft()
-            vis.add((r,c))
+            r,c,d=q.popleft()
+            res=d
             for dr, dc in dir:
-                n_i,n_j=r+dr, c+dc
+                n_i,n_j=r+dr,c+dc
                 if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
                     if grid[n_i][n_j]==1 and (n_i,n_j) not in vis:
-                        q.append((n_i,n_j,t+1))
+                        q.append((n_i,n_j,d+1))
                         vis.add((n_i,n_j))
-        
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==1 and (i,j) not in vis:
                     return -1
         
-        return t
-
-
+        return res
 
         
