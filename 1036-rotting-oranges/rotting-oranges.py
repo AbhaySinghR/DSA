8a4a4fe78dc2ee5cbb4a7acf10e4b0756f295838
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-03-07 17:19:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (94.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -5,10 +5,9 @@ class Solution:
         n=len(grid[0])
         q=deque()
         res=0
-        vis=set()
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==2 and (i,j) not in vis:
+                if grid[i][j]==2:
                     q.append((i,j,0))
         
         dir=[(1,0),(0,1),(-1,0),(0,-1)]
@@ -18,13 +17,13 @@ class Solution:
             for dr, dc in dir:
                 n_i,n_j=r+dr,c+dc
                 if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
-                    if grid[n_i][n_j]==1 and (n_i,n_j) not in vis:
+                    if grid[n_i][n_j]==1:
                         q.append((n_i,n_j,d+1))
-                        vis.add((n_i,n_j))
+                        grid[n_i][n_j]=2
         
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==1 and (i,j) not in vis:
+                if grid[i][j]==1:
                     return -1
         
         return res
