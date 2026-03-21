f9088f5796e9e409f6ec31d6e1eedf74dbb49667
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-03-21 16:53:47 -0400

    Time: 144 ms (35.99%), Space: 22.5 MB (36.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,28 +1,28 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
 
-
-        n=len(mat)
-        m=len(mat[0])
+        m=len(mat)
+        n=len(mat[0])
         q=deque()
-        visited=set()
-        for i in range(n):
-            for j in range(m):
+        vis=set()
+        for i in range(m):
+            for j in range(n):
                 if mat[i][j]==0:
                     q.append((i,j,0))
-                    visited.add((i,j))
-        
+                    vis.add((i,j))
+
+
+        dir=[(0,1),(1,0),(-1,0),(0,-1)]
         while q:
-            curr_i,curr_j, dis= q.popleft()
-            dir=[(0,1),(1,0),(0,-1),(-1,0)]
-            for i,j in dir:
-                new_i = i + curr_i
-                new_j = j + curr_j
-                if ((0<=new_i<n) and (0<=new_j<m)):
-                    if (new_i,new_j) not in visited:
-                        mat[new_i][new_j]=dis+1
-                        q.append((new_i,new_j,dis+1))
-                        visited.add((new_i,new_j))
-        
+            r,c,d=q.popleft()
+            if mat[r][c]==1:
+                mat[r][c]=d
+            for dr,dc in dir:
+                n_i,n_j=r+dr,c+dc
+                if (n_i>=0 and n_i<m) and (n_j>=0 and n_j<n):
+                    if mat[n_i][n_j]==1 and (n_i,n_j) not in vis:
+                        q.append((n_i,n_j,d+1))
+                        vis.add((n_i,n_j))
+
+
         return mat
-        
