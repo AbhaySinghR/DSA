ebdfa7487c40020eb03353973b07da8312e98511
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-03-30 20:27:31 -0400

    Time: 11 ms (84.64%), Space: 22.3 MB (54.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        hmap={}
 
-        for i in range(len(strs)):
-            s="".join(sorted(strs[i]))
+        hmap={}
+        for i in strs:
+            s=''.join(sorted(i))
             if s not in hmap:
                 hmap[s]=[]
-            hmap[s].append(strs[i])
+            hmap[s].append(i)
+        
         li=[]
         for j in hmap.values():
             li.append(j)
         
         return li
-
-            
         
-            
