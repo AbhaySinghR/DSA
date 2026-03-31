af37c021dc7d39ed07c6a5afb77a81b4796ed13f
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-03-30 20:59:49 -0400

    Time: 7 ms (50.35%), Space: 23.1 MB (36.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,15 +7,13 @@ class Solution:
             hmap[nums[i]]=hmap.get(nums[i],0)+1
         
         heap=[]
-
-        for i,j in hmap.items():
-            heapq.heappush(heap,(-j,i))
-        
-        print (heap)
         li=[]
-        while k>0:
-            f,ky=heapq.heappop(heap)
-            li.append(ky)
-            k-=1
+        for i,j in hmap.items():
+                heapq.heappush(heap,(j,i))
+
+                if len(heap)>k:
+                    heapq.heappop(heap)
         
+        for i,j in heap:
+            li.append(j)
         return li
