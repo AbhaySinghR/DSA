128f9913979920b2dacd7fcc436316199abfa52b
Abhay Singh <46675371+AbhaySinghR@users.noreply.github.com>
2026-04-26 15:15:00 -0400

    Time: 13 ms (50.51%), Space: 19.3 MB (33.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,14 +8,16 @@ class Solution:
         max_l=1
         while j < len(s):
             if s[j] in hmap:
-                max_l=max(max_l, j-i)
-                pos = hmap[s[j]]
-                i = pos + 1
+                pos=hmap[s[j]]
+                while i <=pos:
+                    del hmap[s[i]]
+                    i+=1
             hmap[s[j]]=j
+            max_l=max(max_l,j-i+1)
             j+=1
-        max_l=max(max_l, j-i)
-        return max_l    
 
+
+        return max_l
 
 
 
